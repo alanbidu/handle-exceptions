4f856d2f325a8ac2eba1fb8025992bd4d668f434
Alanbidu <alanbidu@yahoo.com.br>
2020-04-20 21:50:07 -0300

    Updating Account.java

--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,5 +1,56 @@
 package model.entities;
 
 public class Account {
+	
+	private int number;
+	private String holder;
+	private double balance;
+	private double withdrawLimit;
+	
+	public Account(int number, String holder, double balance, double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
 
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(double amount) {
+		this.balance += amount;
+	}
+		
+	public void withdraw(double amount) {
+		this.balance -= amount;
+	}
 }
